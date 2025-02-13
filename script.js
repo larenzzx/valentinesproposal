87b95ac03bb8679038914ee44ce8602c7ec61f09
larenzzx <larenztabotabo@gmail.com>
2025-02-13 21:00:44 +0800

    third commit

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ function showMessage(response) {
 
     // Update the text content, display the message, and change the image to "dance.gif"
     const yesMessage = document.getElementById("question");
-    yesMessage.textContent = "See you tomorrow myloveee!";
+    yesMessage.textContent = "Yeyyyy see you tomorrow myloveee!";
     yesMessage.style.display = "block";
     yesMessage.style.fontStyle = "normal";
     document.getElementsByClassName("image")[0].src = "images/dance.gif";
